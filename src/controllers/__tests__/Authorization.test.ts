569348357b26934208fa6305b7b579c8ad49ddd9
Aleksei Baryshnikov <baryshnikov.a.m@sberbank.ru>
2021-08-16 14:48:41 +0300

    Add tests for various flows

--- src/controllers/__tests__/Authorization.test.ts
+++ src/controllers/__tests__/Authorization.test.ts
@@ -1,107 +1,109 @@
 import request from "supertest";
 import app from '../../../index';
+import { AuthorizationResponse } from "../../models/AuthorizationResponse";
 import { AuthorizationResponseError } from "../../models/AuthorizationResponseError";
 
 describe('authorization endpoint test', () => {
   /**
-  * This section describes testing of AuthorizationController for authorization code flow
-  */
-  describe('authorization code flow', () => {
-    describe('testing authorization code grant with GET method', () => {
-      test('should return authentication form', done => {
-        request(app)
-          .get('/authorize')
-          .query({
-            response_type: 'code',
-            client_id: 'test',
-            redirect_uri: 'http://test',
-            scope: 'test',
-            state: 'test'
-          })
-          .expect(200)
-          .expect('Location', /authorize/)
-          .end(done);
-      });
-  
-      test('should return authentication form (no state)', done => {
-        request(app)
-          .get('/authorize')
-          .query({
-            response_type: 'code',
-            client_id: 'test',
-            redirect_uri: 'http://test',
-            scope: 'test'
-          })
-          .expect(200)
-          .expect('Location', /authorize/)
-          .end(done);
-      });
+   * This section describes testing of AuthorizationController for authorization code flow
+   */
+  describe('authorization code & implicit flows', () => {
+ 
 
-      test('should return authentication form (implicit)', done => {
-        request(app)
-          .get('/authorize')
-          .query({
-            response_type: 'token',
-            client_id: 'test',
-            redirect_uri: 'http://test',
-            scope: 'test'
-          })
-          .expect(200)
-          .expect('Location', /authorize/)
-          .end(done);
-      });
-  
-      test('missing query param: response_type & should return error "invalid_request"', done => {
-        request(app)
-          .get('/authorize')
-          .query({
-            client_id: 'test',
-            redirect_uri: 'http://test',
-            scope: 'test',
-            state: 'test'
-          })
-          .expect(400, (res: Response) => {
-            const body = res.body as unknown as AuthorizationResponseError;
-            expect(body.error.invalid_request).toBe('parameter response_type is missed');
-          })
-          .end(done);
-      });
-  
-      test('missing query param: client_id & should return error "invalid_request"', done => {
-        request(app)
-          .get('/authorize')
-          .query({
-            response_type: 'code',
-            redirect_uri: 'http://test',
-            scope: 'test',
-            state: 'test'
-          })
-          .expect(400, (res: Response) => {
-            const body = res.body as unknown as AuthorizationResponseError;
-            expect(body.error.invalid_request).toBe('parameter client_id is missed');
-          })
-          .end(done);
-      });
-  
-      test('missing query param: redirect_uri & should return error "invalid_request"', done => {
-        request(app)
-          .get('/authorize')
-          .query({
-            response_type: 'code',
-            client_id: 'test',
-            scope: 'test',
-            state: 'test'
-          })
-          .expect(400, (res: Response) => {
-            const body = res.body as unknown as AuthorizationResponseError;
-            expect(body.error.invalid_request).toBe('parameter redirect_uri is missed');
-          })
-          .end(done);
-      });
-    });
+
   });
+  /**
+   * This section describes testing of AuthorizationController for password credentials flow
+   */
 
-  describe('implicit flow', () => {
+  /**
+   * This section describes testing of AuthorizationController for client credentials flow
+   */
+    describe('password credentials flow', () => {
+      test('should return token', done => {
+        request(app)
+          .post('/authorize')
+          .set('Authorization', Buffer.from('testId:testSecret').toString('base64'))
+          .field('grant_type','password')
+          .field('username','test')
+          .field('password','test')
+          .field('scope','test')
+          .expect(200, (res: Response) => {
+            expect('access_token' in res.body!).toBeTruthy();
+            expect('token_type' in res.body!).toBeTruthy();
+            expect('expires_in' in res.body!).toBeTruthy();
+            expect('refresh_token' in res.body!).toBeTruthy();
+          })
+          .end(done);
+      });
 
+      test('should return token', done => {
+        request(app)
+          .post('/authorize')
+          .set('Authorization', Buffer.from('testId:testSecret').toString('base64'))
+          .field('grant_type','password')
+          .field('username','test')
+          .field('password','test')
+          .expect(200, (res: Response) => {
+            expect('access_token' in res.body!).toBeTruthy();
+            expect('token_type' in res.body!).toBeTruthy();
+            expect('expires_in' in res.body!).toBeTruthy();
+            expect('refresh_token' in res.body!).toBeTruthy();
+          })
+          .end(done);
+      });
+
+      test('should return error with "invalid_request: parameter grant_type is missed"', done => {
+        request(app)
+          .post('/authorize')
+          .set('Authorization', Buffer.from('testId:testSecret').toString('base64'))
+          .field('username','test')
+          .field('password','test')
+          .field('scope','test')
+          .expect(400, (res: Response) => {
+            const body = res.body as unknown as AuthorizationResponseError;
+            expect(body.error.invalid_request).toBe('parameter grant_type is missed');
+          })
+          .end(done);
+      });
+
+      test('should return error with "invalid_request: parameter username is missed"', done => {
+        request(app)
+          .post('/authorize')
+          .set('Authorization', Buffer.from('testId:testSecret').toString('base64'))
+          .field('grant_type','password')
+          .field('password','test')
+          .field('scope','test')
+          .expect(400, (res: Response) => {
+            const body = res.body as unknown as AuthorizationResponseError;
+            expect(body.error.invalid_request).toBe('parameter username is missed');
+          })
+          .end(done);
+      });
+
+      test('should return error with "invalid_request: parameter password is missed"', done => {
+        request(app)
+          .post('/authorize')
+          .set('Authorization', Buffer.from('testId:testSecret').toString('base64'))
+          .field('grant_type','password')
+          .field('username','test')
+          .field('scope','test')
+          .expect(400, (res: Response) => {
+            const body = res.body as unknown as AuthorizationResponseError;
+            expect(body.error.invalid_request).toBe('parameter password is missed');
+          })
+          .end(done);
+      });
+
+      test('should return 401 error', done => {
+        request(app)
+          .post('/authorize')
+          .set('Authorization', Buffer.from('testId:testSecret').toString('base64'))
+          .field('grant_type','password')
+          .field('username','test')
+          .field('password','test')
+          .expect(401)
+          .end(done);
+      });
   });
 });
